d7b56e3366f78a4065ac0b03e5e36337b4e17639
Webbeuker-Martien <martien@webbeukers.nl>
2024-05-11 14:04:00 +0200

    Move & copy file endpoints

--- controllers/dir.js
+++ controllers/dir.js
@@ -125,19 +125,155 @@ export const post = async (req, res, next, withSlashes = false) => {
 						});
 					}
 				} catch (err) {
-					console.log(err);
-
 					if (err.code === 'ENOENT') {
 						// Add file
 						file.mv(basePath + '/' + file.name, (err) => {
 							if (err) {
-								console.log(err);
+								console.error(err);
+
+								return res.status(400).json({
+									success: false,
+									message: 'Failed to upload file'
+								});
 							}
 						});
 
 						return res.status(200).json({
 							success: true,
 							message: 'File uploaded succesfully'
+						});
+					}
+
+					console.error(err);
+					return res.status(500).json({
+						success: false,
+						message: 'Internal server error'
+					});
+				}
+			})
+		);
+	} catch (err) {
+		console.error('Error: ', err);
+
+		if (err.code === 'ENOENT') {
+			return res.status(404).json({
+				success: false,
+				message: 'Directory not found'
+			});
+		} else if (err.code === 'ENOTDIR') {
+			return res.status(400).json({
+				success: false,
+				message: 'Not a directory'
+			});
+		} else {
+			return res.status(500).json({
+				success: false,
+				message: 'Internal server error'
+			});
+		}
+	}
+};
+
+export const moveOrCopy = async (req, res, next, action) => {
+	let dir = req.params[0];
+	let dest = req.query.dest;
+
+	if (!dest) {
+		return res.status(400).json({
+			success: false,
+			message: 'No destionation passed'
+		});
+	}
+
+	try {
+		let basePath = ENV.BASE_PATH + dir;
+
+		if (dest) {
+			dest = path.join(ENV.BASE_PATH, dest);
+		}
+
+		const directory = await fs.stat(basePath, { withFileTypes: true });
+
+		if (!directory.isDirectory()) {
+			return res.status(400).json({
+				success: false,
+				message: 'Not a directory'
+			});
+		}
+
+		// File upload
+		if (!req.files) {
+			return res.status(400).json({
+				success: false,
+				message: 'No files provided'
+			});
+		}
+
+		await Promise.all(
+            Object.keys(req.files).map(async (key) => {
+				let file = req.files[key];
+				let fileExists = null;
+
+				try {
+					fileExists = await fs.stat(path.join(basePath, file.name));
+
+					if (fileExists.isFile()) {
+						let fileInDestExists = null;
+						
+						try {
+							fileInDestExists = await fs.stat(path.join(dest, file.name));
+
+							if (fileInDestExists.isFile()) {
+								// Ask user to overwrite or not
+								return res.status(400).json({
+									success: false,
+									message: 'File already exists (if you want to override, pass param)'
+								});
+							}
+						} catch (err) {
+							if (err.code === 'ENOENT') {
+								// Add file to dest
+								file.mv(path.join(dest, file.name), async (err) => {
+									if (err) {
+										console.error(err);
+
+										return res.status(400).json({
+											success: false,
+											message: `Failed to ${action} file`
+										});
+									}
+
+									if (action === 'move') {
+										// Remove file from basePath
+										await fs.unlink(path.join(basePath, file.name));
+									}
+								});
+
+								return res.status(200).json({
+									success: true,
+									message: `File ${action === 'move' ? 'moved' : action === 'copy' ? 'copied' : 'handled'} succesfully`
+								});
+							}
+
+							console.error(err);
+							return res.status(500).json({
+								success: false,
+								message: 'Internal server error'
+							});
+						}
+					} else {
+						return res.status(400).json({
+							success: false,
+							message: 'File doesn\'t exist'
+						})
+					}
+				} catch (err) {
+					console.error(err);
+
+					if (err.code === 'ENOENT') {
+						return res.status(400).json({
+							success: false,
+							message: 'No such file or directory'
 						});
 					}
 
@@ -173,5 +309,6 @@ export const post = async (req, res, next, withSlashes = false) => {
 export default {
 	getAll,
 	get,
-	post
+	post,
+	moveOrCopy
 };
--- routes/dir.js
+++ routes/dir.js
@@ -9,9 +9,21 @@ router.get('/', dir.getAll);
 router.get('/:dir', dir.get);
 router.get('/*', (req, res, next) => dir.get(req, res, next, true));
 
+// Upload file
 router.post('/*',
     fileUpload({ createParentPath: true }),
     (req, res, next) => dir.post(req, res, next, true)
+);
+
+// Move file
+router.put('/move/*',
+    fileUpload({ createParentPath: true }),
+    (req, res, next) => dir.moveOrCopy(req, res, next, 'move')
+);
+
+router.put('/copy/*',
+    fileUpload({ createParentPath: true }),
+    (req, res, next) => dir.moveOrCopy(req, res, next, 'copy')
 );
 
 export default router;
